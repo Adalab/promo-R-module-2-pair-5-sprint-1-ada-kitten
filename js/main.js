b9b0adb70377b98a0ba96a5d78c0ff1e2c62ef32
Maria SJ <maria.som.jur@gmail.com>
2022-10-20 10:47:30 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict"
 
-const visibleForm = document.querySelector(".js-new-form");
-visibleForm.classList.remove("collapsed");
+//const visibleForm = document.querySelector(".js-new-form");
+//visibleForm.classList.remove("collapsed");
 
 const dataAddClass = document.querySelector(".list");
 dataAddClass.classList.add("js-list");
@@ -15,4 +15,24 @@ const kittenTwo = '<li class="card"><img class="card_img" src="https://images.em
 const kittenThree = '<li class="card"><img class="card_img" src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"alt="gatito"/><h3 class="card_title">Cielo</h3><h4 class="card_race">British Shorthair</h4><p class="card_description">Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p></li>'; 
 
 jsListClass.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+const input_search_desc = document.querySelector(".js_in_search_desc");
+
+input_search_desc.value = "cariñoso";
+
+const descrSearchText = input_search_desc.value;
+
+if( kittenDesc1.includes(descrSearchText) ) {
+    //Completa el código
+    }
+    
+    if( kittenDesc2.includes(descrSearchText) ) {
+    //Completa el código
+    }
+    
+    if( kittenDesc3.includes(descrSearchText) ) {
+    //Completa el código
+    }
+    
+
 
